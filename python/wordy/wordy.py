cf4d444ff3996c654ed1ccfb61837bc0e819613a
Lawrence Olaiya <lawrence.olaiya@andela.com>
2016-06-03 01:33:50 -0400

    Move `get_next_token` function out of the `try` block.

--- python/wordy/wordy.py
+++ python/wordy/wordy.py
@@ -14,10 +14,10 @@ def calculate(word_problem):
     filtered_word_problem = re.sub(r'({0})'.format(stop_words), '', word_problem, flags=re.I)
     tokens = list(filter(lambda token: bool(token) == True, filtered_word_problem.split(' ')))
     tokens.reverse()
+    get_next_token = tokens.pop
 
     def do_operation():  # requires three tokens to execute each time.
         try:
-            get_next_token = tokens.pop
             operand_1 = int(get_next_token())
             _operator = get_next_token()
             operand_2 = int(get_next_token())
