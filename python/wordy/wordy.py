f9395ddca6fb154b12693b10f603f1bf6f472ad4
Lawrence Olaiya <lawrence.olaiya@andela.com>
2016-06-03 01:26:28 -0400

    Badass wordy solution using stack implementation.

--- python/wordy/wordy.py
+++ python/wordy/wordy.py
@@ -1,42 +1,30 @@
 import re
 import operator
 
-ops_map = {
+ops = {
     'plus': operator.add,
     'minus': operator.sub,
-    'multiplied by': operator.mul,
-    'divided by': operator.floordiv,
+    'multiplied': operator.mul,
+    'divided': operator.floordiv,
 }
-ops_choices = '|'.join(ops_map.keys())
-
-
-def extract_data(regex, word_problem):
-    print(regex)
-    result = re.search(regex, word_problem, flags=re.I)
-    print(result)
-    return result.groups() if result else None
-
-
-def calculate_a_op_b(a, op, b):
-    if op in ops_map:
-        return ops_map[op](int(a), int(b))
-
-
-def calculate_a_op_b_op_c(a, op1, b, op2, c):
-    if op1 in ops_map and op2 in ops_map:
-        partial_result = ops_map[op1](int(a), int(b))
-        return ops_map[op2](partial_result, int(c))
 
 
 def calculate(word_problem):
-    for regex, fn in regex_map:
-        data = extract_data(regex, word_problem)
-        if data is not None:
-            return fn(*data)
-    raise ValueError()
+    stop_words = 'what|which|greater|is|by|,|\?'
+    filtered_word_problem = re.sub(r'({0})'.format(stop_words), '', word_problem, flags=re.I)
+    tokens = list(filter(lambda token: bool(token) == True, filtered_word_problem.split(' ')))
+    tokens.reverse()
 
-
-regex_map = [
-    (r'(-?\d+) ({0}) (-?\d+) ({0}) (-?\d+)'.format(ops_choices), calculate_a_op_b_op_c),
-    (r'(-?\d+) ({0}) (-?\d+)'.format(ops_choices), calculate_a_op_b)
-]
+    def do_operation():  # requires three tokens to execute each time.
+        try:
+            get_next_token = tokens.pop
+            operand_1 = int(get_next_token())
+            _operator = get_next_token()
+            operand_2 = int(get_next_token())
+            tokens.append(ops[_operator](operand_1, operand_2))
+            if len(tokens) >= 3:
+                do_operation()
+        except Exception:
+            raise ValueError('Problem does\'nt compute!')
+    do_operation()
+    return tokens[0]
